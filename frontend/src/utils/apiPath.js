710d279dd3f24c0f3d22edf8ea38fed800261c10
vasuki-elango <vasukie96@gmail.com>
2025-06-16 16:01:38 +0530

    add .env file

--- frontend/src/utils/apiPath.js
+++ frontend/src/utils/apiPath.js
@@ -1,5 +1,4 @@
-export const BASE_URL = 'http://localhost:5000';
-
+export const BASE_URL = process.env.REACT_APP_BASE_URL 
 export const API_PATHS = {
   AUTH: {
     LOGIN :"/api/auth/login",
--- frontend/src/utils/axiosInstances.js
+++ frontend/src/utils/axiosInstances.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 import { BASE_URL } from "./apiPath";
 
+
 const axiosInstance = axios.create({
-  baseURL: BASE_URL,
+  baseURL: BASE_URL  ,
   timeout: 10000,
     headers: {
         "Content-Type": "application/json",
