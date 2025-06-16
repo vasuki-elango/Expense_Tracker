35bab55c9ee5c8c3cf12b6a7b3a0572aba271d6c
vasuki-elango <vasukie96@gmail.com>
2025-06-16 16:40:22 +0530

    add vercel file

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,14 @@ app.use('/api/income',incomeRoutes)
 app.use('/api/expense',expenseRoutes)
 app.use('/api/dashboard',dashboardRoutes)
 
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from API' });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running on port ${process.env.PORT}`);
 })
